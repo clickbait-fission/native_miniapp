41aee9f4e87f044298923b6d41fad375bb5d3a8d
afpro <admin@afpro.net>
2026-08-25 18:27:24 +0800

    RotateSpan empty structure

--- miniprogram/pages/feed/index.ts
+++ miniprogram/pages/feed/index.ts
@@ -1,7 +1,50 @@
 import {IAppOption} from "../../../typings";
+import {Media} from "../../api";
 
 const app = getApp<IAppOption>();
 const page = '/feed';
+
+type OptMedia = Media | null;
+
+function initData(): Media[] {
+    return [];
+}
+
+function initMedias(): OptMedia[] {
+    return [null, null, null, null, null];
+}
+
+class MediaInSpan {
+    media: Media | null = null;
+    active: boolean = false;
+    nearActive: boolean = false;
+}
+
+class RotateSpan {
+    readonly span: MediaInSpan[] = [
+        new MediaInSpan(),
+        new MediaInSpan(),
+        new MediaInSpan(),
+        new MediaInSpan(),
+        new MediaInSpan(),
+    ];
+
+    get spanActiveIndex(): number {
+        // todo: 当前展示内容在span中的index
+    }
+
+    get spanEnd(): number {
+        // todo: 当前展示内容最后一个media在所有数据中的index
+    }
+
+    move(step: number) {
+        // todo: 移动当前span特定个步骤, 对应用户滑动了n个视频
+    }
+
+    pickData(data: Media[]) {
+        // todo: data是整个feed流的所有数据, 根据当前位置和span的rotate情况, 挑选数据填充到span中
+    }
+}
 
 Component({
     options: {
@@ -22,6 +65,11 @@ Component({
     data: {
         _skChecked: false,
         _skReportPromise: null as Promise<void> | null,
+        direction: 'positive',
+        medias: initMedias(),
+        _data: initData(),
+        _rotateOffset: 0,
+        _dataOffset: 0,
     },
     pageLifetimes: {
         show() {
@@ -37,5 +85,6 @@ Component({
             });
         },
     },
-    methods: {}
+    methods: {
+    }
 })
