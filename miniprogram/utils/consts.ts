700a546cb45d5fde53378135a1330e41fd740511
afpro <admin@afpro.net>
2026-08-26 08:46:19 +0800

    remove dev flag

--- miniprogram/utils/consts.ts
+++ miniprogram/utils/consts.ts
@@ -1,10 +1,10 @@
-export const kDev = true;
-export const kAppId = 3;
-export const kApiBase = "http://192.168.199.205:6001/api";
+export const kDev = false;
 
-// export const kDev = false;
-// export const kAppId = 2;
-// export const kApiBase = "https://api.shannonlimit.cn/api";
+// export const kAppId = 3;
+// export const kApiBase = "http://192.168.199.205:6001/api";
+
+export const kAppId = 2;
+export const kApiBase = "https://api.shannonlimit.cn/api";
 
 export const kHome = '/pages/index/index'
 export const kNickName = "神秘用户";
